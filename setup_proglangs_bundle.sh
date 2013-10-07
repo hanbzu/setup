b8c4dcce3b868aa7c525d77daac330755e1df980
hanbzu <hibai.unzueta@gmail.com>
2013-10-07 22:39:39 +0200

    Installation of SML ready

--- setup_proglangs_bundle.sh
+++ setup_proglangs_bundle.sh
@@ -7,3 +7,20 @@
 sudo add-apt-repository -y ppa:cassou/emacs
 sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+
+if [ ! -d ~/tools/ ]; then
+	mkdir ~/tools
+fi
+
+# SML/NJ requires support for 32 bit executables
+#sudo apt-get install gcc-multilib ia32-libs
+
+# SML: Create the ~/tools/sml directory
+if [ ! -d ~/tools/sml ]; then
+	mkdir ~/tools/sml
+fi
+
+# SML: Download installer and start installation
+cd ~/tools/sml
+curl http://www.smlnj.org/dist/working/110.74/config.tgz | tar -xz
+config/install.sh
