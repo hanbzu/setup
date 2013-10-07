7ea200f7e241670e8cc955e1fb7f1986eb8b586c
hanbzu <hanbzu@kuskurtros>
2013-10-07 18:01:37 +0200

    Experimental instalation of Scada dev-env

--- setup_scala_dev.sh
+++ setup_scala_dev.sh
@@ -7,3 +7,18 @@
 # JDK
 sudo apt-get install openjdk-7-jdk
 
+if [ -d ~/tools/ ]; then
+	mkdir ~/tools
+fi
+
+# SBT
+cd ~/tools
+curl http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.12.4/sbt.tgz | tar -xz
+
+# Get Scala Eclipse IDE from TypeSafe (for Scala version 2.10.2)
+cd ~/tools
+# 32 bit Linux
+curl http://downloads.typesafe.com/scalaide-pack/3.0.1.vfinal-210-20130718/scala-SDK-3.0.1-vfinal-2.10-linux.gtk.x86.tar.gz | tar -xz
+# 64 bit Linux
+#curl http://downloads.typesafe.com/scalaide-pack/3.0.1.vfinal-210-20130718/scala-SDK-3.0.1-vfinal-2.10-linux.gtk.x86_64.tar.gz | tar -xz
+
