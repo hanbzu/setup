9fb7520562cb2a7dc41ae235e9e89c73fe3d791e
hanbzu <hibai.unzueta@gmail.com>
2013-10-11 17:14:51 +0200

    Sputnik setup

---
+++ setup_sputnik.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Some changes made to the XPS 13 computer
+# just after clean istalation of Ubuntu 13.04
+# -------------------------------------------
+# CAUTION: In case you use this script make
+# sure you understand the commands
+# -------------------------------------------
+
+sudo add-apt-repository ppa:canonical-hwe-team/sputnik-kernel
+sudo apt-get update
+sudo apt-get upgrade
