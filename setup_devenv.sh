8285aa3c401e427312d5cf95c69ba57b092d6397
Hibai Unzueta <hibai.unzueta@gmail.com>
2013-10-13 14:34:20 +0200

    Reorganised and new editor setup script

---
+++ setup_devenv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Simple setup.sh for configuring a Ubuntu machine for headless setup.
+
+# Make sure there's a tools directory
+if [ ! -d ~/tools/ ]; then
+	mkdir ~/tools
+fi
+
+cd ~/tools
+curl http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2 | tar -xj
+
+
+
+
+
