34d0adc4237dd5086a72dfd7172a0c86b8c555c5
Hibai Unzueta <hibai.unzueta@gmail.com>
2013-10-15 10:13:53 +0200

    Got some prerrequisites from screencasting blog post

--- setup_studio.sh
+++ setup_studio.sh
@@ -8,7 +8,7 @@
 
 # REMOVING PACKAGES WE WONT USE
 # We will not use Shotwell
-sudo apt-get remove --purge shotwell
+sudo apt-get remove -y --purge shotwell
 
 # ADDING REPOSITORIES
 # Darktable
@@ -20,7 +20,21 @@ sudo add-apt-repository -y ppa:samrog131/ppa
 # Inkscape
 sudo add-apt-repository -y ppa:inkscape.dev/stable
 # ...and now update
-sudo apt-get update
+sudo apt-get -qq update
+
+# Prerrequisites. From here (sept.2013):
+# http://indykish.wordpress.com/2013/09/24/screencast-in-ubuntu-using-openshot/
+sudo apt-get remove -y ffmpeg
+sudo apt-get purge -y ffmpeg
+sudo add-apt-repository -y ppa:jon-severinsson/ffmpeg
+sudo apt-get -qq update
+sudo apt-get install -y ffmpeg
+sudo add-apt-repository -y ppa:sunab/kdenlive-release
+sudo apt-get -qq update
+sudo apt-get install -y libmlt5 libmlt-data libmlt++3 python-mlt5 melt libsox-fmt-all
+sudo apt-get upgrade -y libmlt5 libmlt-data libmlt++3 python-mlt5 melt libsox-fmt-all
+sudo apt-get install -y ladspa-sdk
+sudo apt-get install -y frei0r-plugins
 
 # INSTALLING PACKAGES
 # We will use Darktable instead of Shotwell
@@ -29,6 +43,9 @@ sudo apt-get install -y darktable
 sudo apt-get install -y gimp
 sudo apt-get install -y inkscape
 # Openshot video editor
+rm -r  ~/.openshot
 sudo apt-get install -y openshot openshot-doc
 # Qgifer: gifs out of videos
 sudo apt-get install -y qgifer
+# Blender
+sudo apt-get install blender
