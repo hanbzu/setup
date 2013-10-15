25e75a6ef5e83246a5c068d72e3f6bdc54ef85c6
Hibai Unzueta <hibai.unzueta@gmail.com>
2013-10-15 16:03:24 +0200

    Added caffeine

--- setup_sputnik.sh
+++ setup_sputnik.sh
@@ -6,12 +6,17 @@
 # sure you understand the commands
 # -------------------------------------------
 
-sudo add-apt-repository ppa:canonical-hwe-team/sputnik-kernel
-sudo apt-get update
-sudo apt-get upgrade
+sudo add-apt-repository -y ppa:canonical-hwe-team/sputnik-kernel
+sudo apt-get update -qq
+sudo apt-get upgrade -y
 
 # We won't be using Thunderbird 
 sudo apt-get remove --purge thunderbird*
 
 # For now, we'll be using Chrome instead of Firefox
 sudo apt-get install google-chrome-stable
+
+# Install 'Caffeine' which prevents the OS from suspending while whatching videos
+sudo add-apt-repository -y ppa:caffeine-developers/caffeine-dev
+sudo apt-get update -qq
+sudo apt-get install -y caffeine
