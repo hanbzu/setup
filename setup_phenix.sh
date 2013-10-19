bceb72fc16f1bebe35e3b9543efd74078439c058
Hibai Unzueta <hibai.unzueta@gmail.com>
2013-10-19 22:52:36 +0200

    Added Phenix

---
+++ setup_phenix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p ~/bin
+cd ~/bin
+tar xvf ~/phenix-installer-1.8.4-1496-intel-linux-2.6-x86_64-fc3.tar
+cd ~/bin/phenix-installer-1.8.4-1496
+sudo ./install --prefix=/home/$USER/bin/
+echo "You may want to delete now the folder ~/bin/phenix-installer-1.8.4-1496"
