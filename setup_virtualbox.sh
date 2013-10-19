0ee715fb026b23b329149013627e5b76412e24b7
Hibai Unzueta <hibai.unzueta@gmail.com>
2013-10-19 22:20:23 +0200

    Virtualbox setup

--- setup_virtualbox.sh
+++ setup_virtualbox.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
 
+sudo apt-get install dkms
 sudo apt-get install -y virtualbox-ose
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+CODENAME=`lsb_release --codename | cut -f2` # Ubuntu codename
+echo "deb http://download.virtualbox.org/virtualbox/debian $CODENAME contrib" | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
+sudo apt-get update -qq
+sudo apt-get install -y virtualbox-4.3
