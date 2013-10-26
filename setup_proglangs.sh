9250d4d8bad850696570007bc0ae6b0199dc4df5
Hibai Unzueta <hibai.unzueta@gmail.com>
2013-10-26 18:56:47 +0200

    Minor tweaks

--- setup_proglangs.sh
+++ setup_proglangs.sh
@@ -9,7 +9,7 @@ sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 
 # SML/NJ requires support for 32 bit executables
-sudo apt-get install gcc-multilib ia32-libs
+sudo apt-get install -y gcc-multilib ia32-libs
 
 # SML: Create the ~/bin/sml directory, download and install
 mkdir -p ~/bin/sml
@@ -18,10 +18,10 @@ curl http://www.smlnj.org/dist/working/110.74/config.tgz | tar -xz
 config/install.sh
 
 # SML mode for emacs
-sudo apt-get install sml-mode
+sudo apt-get install -y sml-mode
 
 # Racket from repos
-sudo apt-get install racket
+sudo apt-get install -y racket
 
 # Install RVM for Ruby development
 curl -L https://get.rvm.io | bash -s stable --ruby=1.9.3
