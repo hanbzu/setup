c0795c9a7e59b49bd003791560eb1a410d5617a7
Hibai Unzueta <hibai.unzueta@gmail.com>
2013-10-26 18:25:12 +0200

    Fonts and other tweaks

--- setup_sputnik.sh
+++ setup_sputnik.sh
@@ -21,11 +21,12 @@ CODENAME=`lsb_release --codename | cut -f2`
 #sudo apt-get remove --purge thunderbird*
 
 # For now, we'll be using Chrome instead of Firefox
+sudo apt-get install -y libxss1
 cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 # Fix errors encountered during install
-sudo apt-get -f install
+sudo apt-get -f -y install
 
 # Install 'Caffeine' which prevents the OS from suspending while whatching videos
 #sudo add-apt-repository -y ppa:caffeine-developers/caffeine-dev
@@ -36,29 +37,36 @@ sudo apt-get -f install
 gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
 
 # Basic apps
-sudo apt-get install -y filezilla
+sudo apt-get install -y filezilla fslint
+
+# Inconsolata: Useful font for coding and console
+sudo apt-get install ttf-inconsolata
 
 # Spell cheking and dictionaries for EU, CA, EN-UK, DE, ES, FR
 # More info at: http://packages.ubuntu.com/lucid/hunspell-dictionary
 # Useful way of checking what's need:
 # check-language-support -l fr
 # check-language-support -l en_GB
-sudo apt-get install libmythes-1.2-0 hunspell
-sudo apt-get install hunspell-eu-es
-sudo apt-get install hunspell-de-de mythes-de wngerman wogerman
-sudo apt-get install myspell-ca mythes-ca wcatalan
-sudo apt-get install myspell-es
-sudo apt-get install hunspell-fr mythes-fr hyphen-fr wfrench
+sudo apt-get install -y libmythes-1.2-0 hunspell
+sudo apt-get install -y hunspell-eu-es
+sudo apt-get install -y hunspell-de-de mythes-de wngerman wogerman
+sudo apt-get install -y myspell-ca mythes-ca wcatalan
+sudo apt-get install -y myspell-es
+sudo apt-get install -y hunspell-fr mythes-fr hyphen-fr wfrench
 
 # Skype. We need to add Canonical partner repositories
 sudo sh -c "echo 'deb http://archive.canonical.com/ubuntu/ raring partner' >> /etc/apt/sources.list.d/canonical_partner.list"
 sudo apt-get update -qq
-sudo apt-get install skype
+sudo apt-get install -y skype
 
 # Virtualbox
-sudo apt-get install dkms
+sudo apt-get install -y dkms
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
 CODENAME=`lsb_release --codename | cut -f2` # Ubuntu codename
 echo "deb http://download.virtualbox.org/virtualbox/debian $CODENAME contrib" | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
 sudo apt-get update -qq
 sudo apt-get install -y virtualbox-4.3
+echo "WARNING: Maybe this does not work for Saucy (13.10) because the PPA has not been updated."
+echo "A workaround would be to change in etc/apt/sources.list.d/virtualbox.list"
+echo "the line that says 'saucy' to 'raring'"
+
