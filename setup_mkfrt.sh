60fe26e71056a38f0336bd4aab0368959436d17c
Hibai Unzueta <hibai.unzueta@gmail.com>
2013-12-26 13:44:13 +0100

    Only cosmetic

--- setup_mkfrt.sh
+++ setup_mkfrt.sh
@@ -9,33 +9,34 @@
 
 # Color messages
 MSGCOL="\033[37;44m"
+ENDCOL="\033[0m"
 
 # Basic for automatic installs
-echo -e "$MSGCOL Preparing for LUBUNTU MKFRT setup "
+echo -e "$MSGCOL Preparing for LUBUNTU MKFRT setup $ENDCOL"
 sudo apt-get install -y git
 sudo apt-get install -y curl
 sudo apt-get update -qq
 sudo apt-get upgrade -y
 
 # Desktop apps
-#echo -e "$MSGCOL Desktop apps "
+#echo -e "$MSGCOL Desktop apps $ENDCOL"
 #source ~/setup/setup_desktop.sh
 
 # Removing unnecessary packages from this Lubuntu distribution
 # << lubuntu-desktop is just a metapackage that install all lubuntu-related packages in a single step >>
 # http://askubuntu.com/questions/317464/can-i-safely-remove-lubuntu-desktop
-echo -e "$MSGCOL Removing unnecessary packages from Lubuntu "
+echo -e "$MSGCOL Removing unnecessary packages from Lubuntu $ENDCOL"
 sudo apt-get remove -y lubuntu-desktop
 sudo apt-get purge -y ace-of-penguins abiword gnumeric
 
 # LibreOffice 4.1 (only that version in PPA)
 # << It should be noted that this ppa will only produce updates for the 4.1 series >>
 # http://askubuntu.com/questions/252612/how-do-i-install-libreoffice-4
-echo -e "$MSGCOL Installing LibreOffice 4.1 (PPA) "
+echo -e "$MSGCOL Installing LibreOffice 4.1 (PPA) $ENDCOL"
 sudo add-apt-repository -y ppa:libreoffice/libreoffice-4-1
 sudo apt-get update -qq
 sudo apt-get install -y libreoffice
 
 # Synapse is a fast app launcher
-echo -e "$MSGCOL Installing Synapse "
+echo -e "$MSGCOL Installing Synapse $ENDCOL"
 sudo apt-get install -y synapse
