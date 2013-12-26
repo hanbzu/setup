a92d7b4136eefd383171e06f960b948383b8e131
Hibai Unzueta <hibai.unzueta@gmail.com>
2013-12-26 19:48:09 +0100

    In situ testing for mkfrt

--- setup_mkfrt.sh
+++ setup_mkfrt.sh
@@ -21,37 +21,37 @@ sudo apt-get update -qq
 sudo apt-get upgrade -y
 
 # Desktop apps
-#echo -e "$MSGCOL Desktop apps $ENDCOL"
-#source ~/setup/setup_desktop.sh
+echo -e "$MSGCOL Desktop apps $ENDCOL"
+source ~/setup/setup_desktop.sh
 
 # Removing unnecessary packages from this Lubuntu distribution
 # << lubuntu-desktop is just a metapackage that install all lubuntu-related packages in a single step >>
 # http://askubuntu.com/questions/317464/can-i-safely-remove-lubuntu-desktop
-#echo -e "$MSGCOL Removing unnecessary packages from Lubuntu $ENDCOL"
-#sudo apt-get remove -y lubuntu-desktop
-#sudo apt-get purge -y ace-of-penguins abiword gnumeric
+echo -e "$MSGCOL Removing unnecessary packages from Lubuntu $ENDCOL"
+sudo apt-get remove -y lubuntu-desktop
+sudo apt-get purge -y ace-of-penguins abiword gnumeric
 
 # LibreOffice 4.1 (only that version in PPA)
 # << It should be noted that this ppa will only produce updates for the 4.1 series >>
 # http://askubuntu.com/questions/252612/how-do-i-install-libreoffice-4
-#echo -e "$MSGCOL Installing LibreOffice 4.1 (PPA) $ENDCOL"
-#sudo add-apt-repository -y ppa:libreoffice/libreoffice-4-1
-#sudo apt-get update -qq
-#sudo apt-get install -y libreoffice
+echo -e "$MSGCOL Installing LibreOffice 4.1 (PPA) $ENDCOL"
+sudo add-apt-repository -y ppa:libreoffice/libreoffice-4-1
+sudo apt-get update -qq
+sudo apt-get install -y libreoffice
 
 # Synapse is a fast app launcher
-echo -e "$MSGCOL Installing Synapse $ENDCOL"
-sudo apt-get install -y synapse
-echo "synapse --startup" >> ~/.config/lxsession/Lubuntu/autostart
+#echo -e "$MSGCOL Installing Synapse $ENDCOL"
+#sudo apt-get install -y synapse
+#echo "synapse --startup" >> ~/.config/lxsession/Lubuntu/autostart
 
 # RAW image support
-echo -e "$MSGCOL Installing support for RAW images $ENDCOL"
-sudo apt-get install -y ufraw gimp-ufraw ufraw-batch
+#echo -e "$MSGCOL Installing support for RAW images $ENDCOL"
+#sudo apt-get install -y ufraw gimp-ufraw ufraw-batch
 
 # Dropbox
-echo -e "$MSGCOL Installing dropbox $ENDCOL"
-source ~/setup/setup_dropbox.sh
-echo "dropbox start -i" >> ~/.config/lxsession/Lubuntu/autostart
+#echo -e "$MSGCOL Installing dropbox $ENDCOL"
+#source ~/setup/setup_dropbox.sh
+#echo "dropbox start -i" >> ~/.config/lxsession/Lubuntu/autostart
 
 # Autostart
 # IMPORTANT: This is managed under Lubuntu Saucy
