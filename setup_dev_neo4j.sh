b0f55b0b20317b60a709ab1901493d435d1e7106
Hibai Unzueta <hibai.unzueta@gmail.com>
2014-01-27 23:03:27 +0100

    Modified for neo4j installation

--- setup_dev_neo4j.sh
+++ setup_dev_neo4j.sh
@@ -3,7 +3,7 @@
 # Import our signing key
 wget -O - http://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add - 
 # Create an Apt sources.list file
-echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
+sudo echo 'deb http://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
 # Find out about the files in our repository
 sudo apt-get update -qq
 # Install Neo4j, community edition
