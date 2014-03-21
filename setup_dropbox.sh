3989b04c5998bd2682040c779cf80b6b4af79cc0
Hibai Unzueta <hibai.unzueta@gmail.com>
2014-03-21 23:48:24 +0100

    Added indicator for Dropbox

--- setup_dropbox.sh
+++ setup_dropbox.sh
@@ -6,6 +6,9 @@
 
 CODENAME=`lsb_release --codename | cut -f2`
 
+# For the indicator
+sudo apt-get install libappindicator1 -y
+
 echo "deb http://linux.dropbox.com/ubuntu $CODENAME main">>'dropbox.list'
 chmod 644 'dropbox.list'
 sudo chown root:root 'dropbox.list'
@@ -13,4 +16,3 @@ sudo mv 'dropbox.list' '/etc/apt/sources.list.d/dropbox.list'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5044912E
 sudo apt-get update -qq
 sudo apt-get install dropbox -y
-
