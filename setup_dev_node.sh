ddd0c3ea7f4a4618e26dfdaca4b45105e304c356
Hibai Unzueta <hibai.unzueta@gmail.com>
2014-05-21 17:55:50 +0200

    Fixed some node problems

--- setup_dev_node.sh
+++ setup_dev_node.sh
@@ -14,13 +14,13 @@ sudo apt-get install -y curl
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+nvm install v0.10.28
+nvm use v0.10.28
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
 echo -e "$MSGCOL JsHint (for Emacs) $ENDCOL"
-npm install -g jshint
+sudo npm install -g jshint
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
