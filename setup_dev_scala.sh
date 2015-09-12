61c412db9d7cfcadd9a781c3efb318c51ef4a152
Hibai Unzueta <hibai.unzueta@gmail.com>
2015-09-12 14:13:35 +0200

    Updates in scala installs

--- setup_dev_scala.sh
+++ setup_dev_scala.sh
@@ -9,19 +9,30 @@ sudo apt-get install -y curl
 
 # JDK
 sudo apt-get install -y openjdk-7-jdk
+# openjdk-8 is not available for 14.04 only later versions, so use repo
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update -qq
+sudo apt-get install -y openjdk-8-jdk
+# Choose java alternatives
+sudo update-alternatives --config java
 
 # SBT
 # remove sbt:>  sudo apt-get purge sbt.
-SBT_VERSION=0.13.7
-cd ~/Downloads
-wget http://dl.bintray.com/sbt/debian/sbt-$SBT_VERSION.deb
-sudo dpkg -i sbt-$SBT_VERSION.deb
-rm ~/Downloads/sbt-$SBT_VERSION.deb
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+sudo apt-get update
+sudo apt-get install sbt
+# Old method:
+#SBT_VERSION=0.13.7
+#cd ~/Downloads
+#wget http://dl.bintray.com/sbt/debian/sbt-$SBT_VERSION.deb
+#sudo dpkg -i sbt-$SBT_VERSION.deb
+#rm ~/Downloads/sbt-$SBT_VERSION.deb
 
 # Scala-lang
 # http://downloads.typesafe.com/scala/2.11.0/scala-2.11.0.deb
 # But it changes according to version
-SCALA_VERSION=2.11.4
+SCALA_VERSION=2.11.7
 sudo apt-get remove scala-library scala
 cd ~/Downloads
 wget http://downloads.typesafe.com/scala/$SCALA_VERSION/scala-$SCALA_VERSION.deb
@@ -54,16 +65,16 @@ sudo cp ~/setup/assets/scalaide.desktop /usr/share/applications/
 
 # TuxJDK https://code.google.com/p/tuxjdk/ is an alternative JVM for Linux
 # It is made for Linux developers and I download it because it solves some font issues with IntelliJ IDEA
-sudo mkdir -p /usr/lib/jvm
-cd /usr/lib/jvm
-TUX_JVM_VER=jdk-8u25-tuxjdk-b01
-URL=https://googledrive.com/host/0B68yuEpDuq6waUl5UjNTUWRlYTQ/${TUX_JVM_VER}.tar.xz
-echo ${URL}
-curl -L ${URL} | sudo tar -xJ
-# The JDK will be in /usr/lib/jvm/${TUX_JVM_VER}
+# See here:
+# http://askubuntu.com/questions/454361/font-in-intellij-idea-on-ubuntu-14-04
+TUX_JVM_VER=jdk-8u5-tuxjdk-b08
+wget http://urshulyak.com:85/${TUX_JVM_VER}.tar.gz
+tar -zxvf ${TUX_JVM_VER}.tar.gz
+sudo mv ${TUX_JVM_VER} /usr/lib/jvm
+rm ${TUX_JVM_VER}.tar.gz
 
 # Install IntelliJ IDEA from JetBrains
-IDEA_VER=14.0.3
+IDEA_VER=14.1.4
 mkdir -p ~/Downloads/idea_install
 cd ~/Downloads/idea_install
 curl http://download-cf.jetbrains.com/idea/ideaIC-${IDEA_VER}.tar.gz | tar -xz
@@ -75,17 +86,13 @@ sudo ln -s /opt/idea/bin/idea.sh /usr/local/bin/idea
 sudo cp ~/setup/assets/idea.desktop /usr/share/applications/
 # Once everything is setup, a scala plugin needs to be added through the IDEA interface.
 # Additionally, if you want to correct the poor appearance under Ubuntu Linux,
-# Once TuxJDK has been installed in /usr/lib/jvm you need to change a line in /opt/idea/bin/idea.sh:
-# FROM this:
-# eval "$JDK/bin/java" $ALL_JVM_ARGS -Djb.restart.code=88 $MAIN_CLASS_NAME "$@"
-# TO this:
-# eval "/usr/lib/jvm/jdk-8u25-tuxjdk-b01/bin/java" $ALL_JVM_ARGS -Djb.restart.code=88 $MAIN_CLASS_NAME "$@"
-# Please update the TuxJDK version accordingly
+# Follow the instructions here:
+# http://askubuntu.com/questions/454361/font-in-intellij-idea-on-ubuntu-14-04
 
 # Typesafe activator
 mkdir -p ~/bin
 cd ~/bin
-ACTIVATOR_VERSION=1.2.7
+ACTIVATOR_VERSION=1.3.6
 curl -L http://downloads.typesafe.com/typesafe-activator/$ACTIVATOR_VERSION/typesafe-activator-$ACTIVATOR_VERSION.zip -o tmp.zip
 unzip tmp.zip
 rm tmp.zip
